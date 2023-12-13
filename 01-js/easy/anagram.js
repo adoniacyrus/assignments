8a0fda5c5438533d4b4600464554c0b7e5ce3be6
AdoniaC <122971957+adoniacyrus@users.noreply.github.com>
2023-12-13 20:44:16 +0530

    anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,28 @@
 
 function isAnagram(str1, str2) {
 
+  
+
+  let newStr1=str1.toLowerCase().split('').sort().join('');
+  let newStr2=str2.toLowerCase().split('').sort().join('');
+
+  let n1=newStr1.length
+  let n2=newStr2.length
+
+  if (n1!=n2){
+    return false;
+  }
+
+  for (let i=0;i<n1;i++){
+    if (newStr1[i]!=newStr2[i]){
+      return false;
+    }
+    
+  }
+
+  return true;
+
 }
+
 
 module.exports = isAnagram;
